29cd8d5c7ea2e31f023763b1cd9a103d802441e0
sreelakshmi-8729 <srees8729@gmail.com>
2025-05-03 21:35:28 +0530

    delete files

--- src/main/java/InterviewPracticePackage/Main.java
+++
@@ -1,9 +0,0 @@
-package InterviewPracticePackage;
-
- public class Main {
-    public static void main(String[] args) {
-        InterviewPracticeCode p = new InterviewPracticeCode();
-        p.Q6();
-    }
-
-}
--- src/main/java/InterviewPracticeRepeat/Main.java
+++
@@ -1,9 +0,0 @@
-package InterviewPracticeRepeat;
-
-public class Main {
-    public static void main(String[] args) {
-//        PracticeRepeat.Q1();
-        PracticeRepeat.Q2();
-    }
-
-}
